070a0ff27aceb4bbead7b308a5dab5517d2ec2ce
Asyraf Dayan <asyrafdayan02@gmail.com>
2020-04-06 17:28:24 +0800

    Added widgets folder and info_card.dart

---
+++ lib/widgets/info_card.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class InfoCard extends StatelessWidget {
+
+  final String title;
+  final String info;
+
+  const InfoCard(this.title, this.info);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(8.0),
+          topRight: Radius.circular(8.0),
+          bottomLeft: Radius.zero,
+          bottomRight: Radius.zero
+        )
+      ),
+      child: Container(
+        padding: EdgeInsets.all(10.0),
+        decoration: BoxDecoration(
+          border: Border(
+            bottom: BorderSide(width: 10.0, color: Colors.lightBlue.shade900)
+          )
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Text(title),
+            Text(info)
+          ],
+        ),
+      ),
+    );
+  }
+}

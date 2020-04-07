2c36e43998040f1836530a6a7d4444880c3c624a
Asyraf Dayan <asyrafdayan02@gmail.com>
2020-04-07 16:05:41 +0800

    Added syncfusion_flutter_charts: ^18.1.42 and License in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:my_covid/pages/home.dart';
+import 'package:syncfusion_flutter_core/core.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  SyncfusionLicense.registerLicense('@31382e312e30k1Rs00jdCHEZh9ktfpoVOtsfpI/Bj8mIGF7aiMTMaI8=');
+  return runApp(MyApp()); 
+}
 
 class MyApp extends StatelessWidget {
   @override

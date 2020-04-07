b1839d45b54cb71386ef4dfb1abaf6e7cb9efc86
Asyraf Dayan <asyrafdayan02@gmail.com>
2020-04-07 16:13:21 +0800

    Fixed Syncfusion License Key

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:my_covid/pages/home.dart';
 import 'package:syncfusion_flutter_core/core.dart';
 
 void main() {
-  SyncfusionLicense.registerLicense('@31382e312e30k1Rs00jdCHEZh9ktfpoVOtsfpI/Bj8mIGF7aiMTMaI8=');
+  SyncfusionLicense.registerLicense('NT8mJyc2IWhia31ifWN9Z2FoYmF8YGJ8ampqanNiYmlmamlmanMDHmgyICohMjU3KSY/ODIhPTI6PRM0PjI6P30wPD4=');
   return runApp(MyApp()); 
 }
 

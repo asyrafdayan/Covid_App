896defcd4d18913a7e2237ff11f177cef5c68591
Asyraf Dayan <asyrafdayan02@gmail.com>
2020-04-07 16:15:15 +0800

    Added chart to home.dart

---
+++ lib/charts/bar_chart.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
+
+class CovidBarChart extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return SfCartesianChart(
+      primaryXAxis: CategoryAxis(),
+      // Chart title
+      title: ChartTitle(text: 'Half yearly sales analysis'),
+      // Enable legend
+      legend: Legend(isVisible: true),
+      // Enable tooltip
+      tooltipBehavior: TooltipBehavior(enable: true),
+      series: <ChartSeries<SalesData, String>>[
+        LineSeries<SalesData, String>(
+            dataSource: <SalesData>[
+              SalesData('Jan', 35),
+              SalesData('Feb', 28),
+              SalesData('Mar', 34),
+              SalesData('Apr', 32),
+              SalesData('May', 40)
+            ],
+            xValueMapper: (SalesData sales, _) => sales.year,
+            yValueMapper: (SalesData sales, _) => sales.sales,
+            // Enable data label
+            dataLabelSettings: DataLabelSettings(isVisible: true)
+        )
+      ]
+    );
+  }
+}
+
+class SalesData {
+  SalesData(this.year, this.sales);
+
+  final String year;
+  final double sales;
+}
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:my_covid/widgets/info_card.dart';
-import 'package:charts_flutter/flutter.dart';
+import 'package:my_covid/charts/bar_chart.dart';
 
 class MyHomePage extends StatefulWidget {
   @override
@@ -13,19 +13,26 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: ListView(
-          shrinkWrap: true,
-          children: <Widget>[
-            Padding(
-              padding: EdgeInsets.all(5.0),
-              child: InfoCard('Recent Cases', '3793', Colors.blue)),
-            Padding(
-              padding: EdgeInsets.all(5.0),
-              child: InfoCard('Recent Discharges', '236', Colors.lightGreen)),
-            Padding(
-              padding: EdgeInsets.all(5.0),
-              child: InfoCard('Recent Death', '1', Colors.red)),
-          ],
+        child: Container(
+          child: ListView(
+            shrinkWrap: true,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.all(5.0),
+                child: InfoCard('Recent Cases', '3793', Colors.blue)),
+              Padding(
+                padding: EdgeInsets.all(5.0),
+                child: InfoCard('Recent Discharges', '236', Colors.lightGreen)),
+              Padding(
+                padding: EdgeInsets.all(5.0),
+                child: InfoCard('Recent Death', '1', Colors.red)),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 10.0),
+                height: 300.0,
+                child: CovidBarChart(),
+              )
+            ],
+          ),
         ),
       ),
     );
